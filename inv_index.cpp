063f51be4b95ec0d0568a2efada2004a46ef8afa
Andrés Caro <andres.caro.q@gmail.com>
2013-11-05 00:37:00 -0300

    Nothing special.

--- inv_index.cpp
+++ inv_index.cpp
@@ -5,47 +5,64 @@
 
 using namespace std;
 
+hash<string> hash_fn;
+
 set<size_t> fillStopWords();
+void readPages(set<size_t> stopWords);
 
 int main() {
-    set <size_t> stopWords = fillStopWords();
+    set<size_t> stopWords = fillStopWords();
 
     /* Determina un hash a partir de la palabra a leer. */
-    hash<string> hash_fn;
     string word;
     
     /* Se imprime la cantidad de elementos del set. */
     cout << stopWords.size() << endl;
 
-    /* Ciclo que, dada una palabra, retorna true si se encuentra en el set
-     * o false en caso contrario. */
+    /* 
+     * Ciclo que, dada una palabra, retorna true si se encuentra en el set
+     * o false en caso contrario.
+     */
     while (true) {
         cin >> word;
         if (word == "0")
             break;
-        /* El método find retorna un iterador si se encuentra el elemento,
-         * o el iterador de end() en caso contrario. */
-        cout << (stopWords.find( hash_fn(word) ) != stopWords.end()) << endl;
+        /* 
+         * El método find retorna un iterador al elemento si éste es encontrado,
+         * o el iterador que retorna el método end() en caso contrario.
+         */
+        //cout << (stopWords.find( hash_fn(word) ) != stopWords.end()) << endl;
+        readPages(stopWords);
     }
     return 0;
 }
 
 set<size_t> fillStopWords() {
-    ifstream file;
+    ifstream stopWordsFile;
     string line;
 
     /* Set que contendrá las stopWords. */
     set<size_t> stopWords;
 
-    /* Determina un hash a partir de la palabra a leer. */
-    hash<string> hash_fn;
-
-    file.open("sw.txt");
-    if (file.is_open()) {
-        while (getline(file, line)) {
+    stopWordsFile.open("sw.txt");
+    if (stopWordsFile.is_open())
+        while (getline(stopWordsFile, line))
             /* Inserta en el set el hash de la palabra. */
             stopWords.insert( hash_fn(line) );
-        }
-    }
+
     return stopWords;
+}
+
+void readPages(set<size_t> stopWords) {
+    ifstream pagesFile;
+    string line;
+
+    pagesFile.open("pages/users.dcc.uchile.cl.txt");
+
+    if (pagesFile)
+        while (getline(pagesFile, line)) {
+            if (line == "\n")
+                cout << "\\n" << endl;
+            cout << line << endl;
+        }
 }
